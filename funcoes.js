2c3ff61a3d9cd8a93f1900dc2549b5b0c5a634e2
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2024-04-08 21:05:18 -0300

    atualizando.

--- funcoes.js
+++ funcoes.js
@@ -7,3 +7,28 @@ const euro = document.getElementById('euro');
 const iene = document.getElementById('iene');
 const valor = document.getElementById('valor');
 const valor_result = document.getElementById('valor_resultado');
+
+convert.addEventListener('click', function(){
+    if (money.value === vazio.value){
+        var mult = valor.value*0
+        result.style.display = 'block'
+        valor_result.innerHTML = mult.toFixed(2)
+    }
+    else if (money.value === dolar.value){
+        var mult = valor.value*5.07
+        result.style.display = 'block'
+        valor_result.innerHTML = mult.toFixed(2)
+    }
+
+    else if (money.value === euro.value){
+        var mult = valor.value*5.49
+        result.style.display = 'block'
+        valor_result.innerHTML = mult.toFixed(2)
+    }
+
+    else if (money.value === iene.value){
+        var mult = valor.value*0.033
+        result.style.display = 'block'
+        valor_result.innerHTML = mult.toFixed(2)
+    }
+})
